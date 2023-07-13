91d60d8571fd117e1568ef154e823cb17be5838f
Peter Olah <peteolah@gmail.com>
2023-07-13 14:29:15 -0400

    introduced mongo

--- onedocbot.py
+++ onedocbot.py
@@ -4,23 +4,31 @@ import pandas as pd
 import streamlit as st
 import pinecone
 from auth0_component import login_button
-from llama_index import GPTVectorStoreIndex, SimpleDirectoryReader
+from llama_index import GPTListIndex, GPTVectorStoreIndex, SimpleDirectoryReader
 from llama_index import download_loader
 from llama_index.vector_stores import PineconeVectorStore
 from llama_index.storage.storage_context import StorageContext
 from matplotlib import pyplot as plt
 from pandasai.llm.openai import OpenAI
 from typing import Optional, Union
+from llama_index.node_parser import SimpleNodeParser
+from llama_index.storage.docstore import MongoDocumentStore
+from llama_index.storage.index_store.mongo_index_store import MongoIndexStore
+from llama_index.indices.loading import load_index_from_storage
 from streamlit_extras.buy_me_a_coffee import button
 
 documents_folder = "documents"
 pinecone_api_key = os.environ.get("PINECONE_API_KEY")
 csv_llm = OpenAI(api_token=os.environ['OPENAI_API_KEY'])
+MONGO_URI = os.environ["MONGO_URI"]
 document_uploaded = False
 clientId = os.getenv("AUTH0_CLIENT_ID")
 domain = "dev-takuxm4bkqc2jayl.us.auth0.com"
 redirect_uri = 'http://localhost:8501/component/auth0_component.login_button/index.html'
 user_info = None
+
+user_info = login_button(clientId, domain=domain)
+st.write(user_info)
 
 
 def main():
@@ -76,29 +84,20 @@ def remove_file(file_path):
         os.remove(documents_folder + '/' + file_path)
 
 
-@st.cache_resource
 def create_index():
-    index_name = "will put logic here to look up index per user"
-
     if document_uploaded:
         try:
             documents = SimpleDirectoryReader(documents_folder).load_data()
 
-            if user_info is not None and user_info['sub'] is not None:
-                pinecone.init(api_key=pinecone_api_key,
-                              environment="us-west4-gcp-free")
-
-                if index_name in pinecone.list_indexes():
-                    print("Index already exists!!!")
-                    pinecone_index = pinecone.Index(index_name)
-                else:
-                    pinecone.create_index(index_name, dimension=1536)
-                    pinecone_index = pinecone.Index(index_name=index_name)
-
-                vector_store = PineconeVectorStore(
-                    pinecone_index=pinecone_index)
+            if user_info is not None and user_info and user_info['sub'] is not None:
+                nodes = SimpleNodeParser().get_nodes_from_documents(documents)
                 storage_context = StorageContext.from_defaults(
-                    vector_store=vector_store)
+                    docstore=MongoDocumentStore.from_uri(uri=MONGO_URI),
+                    index_store=MongoIndexStore.from_uri(uri=MONGO_URI),
+                )
+
+                storage_context.docstore.add_documents(nodes)
+
                 index = GPTVectorStoreIndex.from_documents(
                     documents, storage_context=storage_context)
             else:
@@ -161,6 +160,19 @@ def create_index_from_pinecone(is_document_uploaded=False):
         st.warning("Upload a document you want to query.")
 
 
+def create_index_from_mongo(is_document_uploaded=False):
+    storage_context = StorageContext.from_defaults(
+        docstore=MongoDocumentStore.from_uri(uri=MONGO_URI),
+        index_store=MongoIndexStore.from_uri(uri=MONGO_URI),
+    )
+
+    docstore = MongoDocumentStore.from_uri(
+        uri=MONGO_URI, db_name="db_docstore")
+    nodes = list(docstore.docs.values())
+    list_index = GPTListIndex(nodes, storage_context=storage_context)
+    return GPTVectorStoreIndex(nodes, storage_context=storage_context)
+
+
 def query_doc(vector_index, query, is_document_uploaded=False):
     # Applies Similarity Algo, Finds the nearest match and
     # take the match and user query to OpenAI for rich response
@@ -209,6 +221,3 @@ if input_text is not None:
 st.caption(
     "Thank you for trying out PeterBot! If you want to support the project, would love your contributions!")
 button(username="profmanager", floating=False, width=221)
-
-user_info = login_button(clientId, domain=domain)
-# st.write(user_info)
